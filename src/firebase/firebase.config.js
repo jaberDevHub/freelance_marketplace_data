3df08c19055264c199f8d4a8fdfb0a9ef89a2037
Jaber Ahmed <jabarahmed332@gmail.com>
2025-07-07 21:59:19 +0600

    updated

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAw0aETuadALjbY7Qth8dIaIO06NPWhMyM",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "freelance-marketplace-data.firebaseapp.com",
   projectId: "freelance-marketplace-data",
   storageBucket: "freelance-marketplace-data.firebasestorage.app",
